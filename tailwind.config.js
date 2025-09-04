65c0e3c45dde181a6faacb5038e460e86cd974f4
Amaxus1 <maxxpetkov@gmail.com>
2025-09-04 19:36:38 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        calm: {
+          DEFAULT: "#f5f7fa",
+          dark: "#e4e9f2"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
